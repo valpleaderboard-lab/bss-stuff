25b3d3344d8f855f44c11c48c18f0812f7020837
valpleaderboard-lab <valpleaderboard@gmail.com>
2026-03-08 07:06:57 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,19 +1,27 @@
-function request(msg)
-	request{
+local HttpService = game:GetService("HttpService")
+
+function send(msg,name)
+	request({
 		Url = "https://discord.com/api/webhooks/1324084236558532701/QhbB-OUxaScbiaItK9N71Ew5X6iWWsfd2nkPyVBzv5XGV4RppWumNOYyqXh5cmKTuPN1",
 		Method = "POST",
-		Headers = {["Content-Type"] = "application/json"},
-		Body = game:GetService("HttpService"):JSONEncode({
-			["content"] = msg
+		Headers = {
+			["Content-Type"] = "application/json"
+		},
+		Body = HttpService:JSONEncode({
+			embeds = {{
+				description = msg,
+				color = 3066993,
+				author = {
+					name = name
+				}
+			}}
 		})
-	}
+	})
 end
 
 local Events = require(game.ReplicatedStorage.Events)
 
 hookfunction(Events.ClientListen, function(v1,v2)
-	request("----- "..v1.." (ClientListen)".." -----")
-	task.wait(1)
-	request(game:GetService("HttpService"):JSONEncode((v2)))
+	send(game:GetService("HttpService"):JSONEncode(v2),v1)
 	return Events.ClientListen
 end)

f181461700578f54f574992cec0c423fd4f6ad84
valpleaderboard-lab <valpleaderboard@gmail.com>
2026-03-08 07:58:51 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -23,7 +23,7 @@ end
 local Events = require(game.ReplicatedStorage.Events)
 
 hookfunction(Events.ClientListen, function(v1,v2)
-	local output = decompile(v2) 
+	local output = debug.getprotos(v2) 
 	
 	return send(output,v1)
 end)
